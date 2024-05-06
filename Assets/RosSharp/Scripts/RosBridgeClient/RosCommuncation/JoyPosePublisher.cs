cc5c566bba1ae9170dda4ee86fa7b01a5da1b146
HandianYang <ych0610765@gmail.com>
2024-05-06 15:39:52 +0800

    v0.1.3-Add joy_pose publisher

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/JoyPosePublisher.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class JoyPosePublisher : UnityPublisher<MessageTypes.Geometry.PoseStamped>
+    {
+        public string FrameId = "Unity";
+        public GameObject controller;
+
+        private MessageTypes.Geometry.PoseStamped message;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            message = new MessageTypes.Geometry.PoseStamped();
+            message.header.frame_id = FrameId;
+        }
+
+        private void Update()
+        {
+            message.header.Update();
+
+            Vector3 controllerTranslation = TransformExtensions.Unity2Ros(controller.transform.position);
+            message.pose.position.x = controllerTranslation.x;
+            message.pose.position.y = controllerTranslation.y;
+            message.pose.position.z = controllerTranslation.z;
+
+            Quaternion controllerRotation = TransformExtensions.Unity2Ros(controller.transform.rotation);
+            message.pose.orientation.x = controllerRotation.x;
+            message.pose.orientation.y = controllerRotation.y;
+            message.pose.orientation.z = controllerRotation.z;
+            message.pose.orientation.w = controllerRotation.w;
+
+            Publish(message);
+        }
+    }
+}
